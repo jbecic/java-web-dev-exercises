ccf0036c18d919fd42b8d3ebe24d440d66013ecc
jasminbecic <becicjasmin1@gmail.com>
2022-01-05 14:40:07 -0600

    removed excess variables and for loop

--- src/org/launchcode/java/studios/CountingCharacters/countingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters/countingCharacters.java
@@ -9,18 +9,12 @@ public class countingCharacters {
     public static void main(String[] args) {
         HashMap<String, Integer> charactersTotal = new HashMap<>();
         Scanner input = new Scanner(System.in);
-        String delims = "\\W+";
         String userInput;
 
         System.out.println("Please enter in a word, phrase, sentance or paragraph:");
         userInput = input.nextLine();
-        userInput = userInput.trim().toLowerCase(Locale.ROOT).replaceAll("\\d", "");
+        userInput = userInput.trim().toLowerCase(Locale.ROOT).replaceAll("\\d", "").replaceAll("\\W+", "");
         input.close();
-        String[] tokens = userInput.split(delims);
-        userInput = "";
-        for (String item : tokens) {
-            userInput += item;
-        }
         char[] charactersInString = userInput.toCharArray();
 
         for (int i = 0; i < charactersInString.length; i++) {
