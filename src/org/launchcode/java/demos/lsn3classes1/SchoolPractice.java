9502ef78a102bad621e73d2737778d0f71a9c61a
jasminbecic <becicjasmin1@gmail.com>
2022-01-13 18:37:58 -0600

    worked on exercises for ch 4

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -5,21 +5,6 @@ import java.util.ArrayList;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
-        Student jasmin = new Student("Jasmin", 1, 1, 4.0);
+        Student jasmin = new Student("Jasmin", 1, 3, 4.0);
     }
-
-    public class Course {
-        String teacher;
-        String course;
-        ArrayList<String> students = new ArrayList<>();
-
-        public Course(String teacher, String course, ArrayList<String> students) {
-            this.teacher = teacher;
-            this.course = course;
-            this.students = students;
-        }
-
-
-    }
-
 }
